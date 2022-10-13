a8a0d232395dde8bd58488e2be67ff16e191801b
Lencodes <contact@lencodes.com>
2022-10-13 13:33:58 +0200

    CLEANUP

--- plugins/OpenPub/OpenPub.php
+++ plugins/OpenPub/OpenPub.php
@@ -52,46 +52,49 @@ add_action('init', function () {
     }
 });
 
-// this is awfull code
-function kiss_openpub_post_type() {
+/**
+ * Register post types
+ */
+function register_kiss_openpub_post_type() {
     register_post_type('kiss_openpub_pub',
         array(
             'labels'      => array(
                 'name'          => __('Publications', 'textdomain'),
                 'singular_name' => __('Publication', 'textdomain'),
             ),
-            //'rest_base' => '/owc/pdc/v1/items',
-            'public'      => true,
-            'has_archive' => true,
-            'show_in_rest' => true,
-            'supports' => array(
+            'public'        => true,
+            'has_archive'   => true,
+            'show_in_rest'  => true,
+            'supports'      => array(
                 'revisions'
             ),
             'capabilities' => array(
-                'publish_posts' => 'publish_publications',
-                'edit_posts' => 'edit_publications',
-                'edit_others_posts' => 'edit_others_publications',
-                'delete_posts' => 'delete_publications',
-                'delete_others_posts' => 'delete_others_publications',
-                'read_private_posts' => 'read_private_publications',
-                'edit_post' => 'edit_publication',
-                'delete_post' => 'delete_publication',
-                'read_post' => 'read_publication'
+                'publish_posts'         => 'publish_publications',
+                'edit_posts'            => 'edit_publications',
+                'edit_others_posts'     => 'edit_others_publications',
+                'delete_posts'          => 'delete_publications',
+                'delete_others_posts'   => 'delete_others_publications',
+                'read_private_posts'    => 'read_private_publications',
+                'edit_post'             => 'edit_publication',
+                'delete_post'           => 'delete_publication',
+                'read_post'             => 'read_publication'
             )
         )
     );
 }
+add_action('init', 'register_kiss_openpub_post_type');
 
-add_action('init', 'kiss_openpub_post_type');
-function kiss_openpub_taxonomie() {
-    // Type of publications
+/**
+ * Register taxonomies
+ */
+function register_kiss_openpub_taxonomies() {
     $labels = array(
         'name'              => _x( 'Types', 'taxonomy general name' ),
         'singular_name'     => _x( 'Type', 'taxonomy singular name' ),
         'search_items'      => __( 'Search Types' ),
         'all_items'         => __( 'All Types' ),
         'parent_item'       => __( 'Parent Type' ),
-        'parent_item_colon' => __( 'Parent Type:' ),
+        'parent_item_colon' => __( 'Parent Type' ),
         'edit_item'         => __( 'Edit Type' ),
         'update_item'       => __( 'Update Type' ),
         'add_new_item'      => __( 'Add New Type' ),
@@ -100,29 +103,27 @@ function kiss_openpub_taxonomie() {
     );
 
     $args   = array(
-        'labels' => $labels,
-        'public' => true,
-        'show_in_rest' => true, // add support for Gutenberg editor
-        'publicly_queryable' => true,
-        'show_in_nav_menus' => true,
-        'show_in_rest' => true,
-        'show_ui' => true,
-        'show_tagcloud' => true,
-        'show_admin_column' => true,
-        'hierarchical' => true, // make it hierarchical (like categories)
-        'rewrite' => true,
-        'query_var' => true,
-        'show_in_quick_edit' => false,
-        'meta_box_cb' => false,
-        'capabilities' => array( 
+        'labels'                => $labels,
+        'public'                => true,
+        'show_in_rest'          => true,
+        'publicly_queryable'    => true,
+        'show_in_nav_menus'     => true,
+        'show_in_rest'          => true,
+        'show_ui'               => true,
+        'show_tagcloud'         => true,
+        'show_admin_column'     => true,
+        'hierarchical'          => true,
+        'rewrite'               => true,
+        'query_var'             => true,
+        'show_in_quick_edit'    => false,
+        'meta_box_cb'           => false,
+        'capabilities'          => array( 
             'assign_terms' => 'assign_types',
          )
         
     );
-
     register_taxonomy( 'openpub-type', [ 'kiss_openpub_pub' ], $args );
 
-    // Relevant skills for employees
     $labels = array(
         'name'              => _x( 'Skills', 'taxonomy general name' ),
         'singular_name'     => _x( 'Skill', 'taxonomy singular name' ),
@@ -137,33 +138,32 @@ function kiss_openpub_taxonomie() {
         'menu_name'         => __( 'Skills' ),
     );
     $args   = array(
-        'labels' => $labels,
-        'public' => true,
-        'show_in_rest' => true, // add support for Gutenberg editor
-        'publicly_queryable' => true,
-        'show_in_nav_menus' => true,
-        'show_in_rest' => true,
-        'show_ui' => true,
-        'show_tagcloud' => true,
-        'show_admin_column' => true,
-        'hierarchical' => true, // make it hierarchical (like categories)
-        'rewrite' => true,
-        'query_var' => true,
-        'show_in_quick_edit' => false,
-        'meta_box_cb' => false,
-        'capabilities' => array( 
+        'labels'                => $labels,
+        'public'                => true,
+        'show_in_rest'          => true,
+        'publicly_queryable'    => true,
+        'show_in_nav_menus'     => true,
+        'show_in_rest'          => true,
+        'show_ui'               => true,
+        'show_tagcloud'         => true,
+        'show_admin_column'     => true,
+        'hierarchical'          => true,
+        'rewrite'               => true,
+        'query_var'             => true,
+        'show_in_quick_edit'    => false,
+        'meta_box_cb'           => false,
+        'capabilities'          => array( 
             'assign_terms' => 'assign_skills',
          )
     );
 
     register_taxonomy( 'openpub_skill', [ 'kiss_openpub_pub'], $args );
 }
-
-add_action( 'init', 'kiss_openpub_taxonomie' );
+add_action( 'init', 'register_kiss_openpub_taxonomies' );
 
 
 /**
- * Programatically fill Skills and Types
+ * Initiate taxonomies values
  */
 function init_publication_types() {
     $publicationTypes = ["Nieuws", "Werkinstructie"];
@@ -172,8 +172,6 @@ function init_publication_types() {
         wp_insert_term($publicationType, "openpub-type");
     }
 }
-
-
 add_action( 'init', 'init_publication_types' );
 
 function init_publication_skills() {
@@ -197,33 +195,29 @@ function init_publication_skills() {
     );
 
     foreach ( $publicationSkills as $publicationSkill ) {
-        wp_insert_term($publicationSkill, "openpub_skill");
+        wp_insert_term( $publicationSkill, 'openpub_skill' );
     }
 }
-
-
 add_action( 'init', 'init_publication_skills' );
 
 /**
- * Create required user role for publication contributor
+ * Create publication contributor role
  */
 if ( !get_role( 'publication_contributor' ) ) {
     add_role( 'publication_contributor', 'Publicatie bijdrager',
         array(
-            'read' => true,
-    
-            'assign_types' => true,
-            'assign_skills' => true,
-
-            'publish_publications' => true,
-            'edit_publications' => true,
-            'edit_others_publications' => true,
-            'delete_publications' => true,
-            'delete_others_publications' => true,
-            'read_private_publications' => true,
-            'edit_publication' => true,
-            'delete_publication' => true,
-            'read_publication' => true,
+            'read'                          => true,
+            'assign_types'                  => true,
+            'assign_skills'                 => true,
+            'publish_publications'          => true,
+            'edit_publications'             => true,
+            'edit_others_publications'      => true,
+            'delete_publications'           => true,
+            'delete_others_publications'    => true,
+            'read_private_publications'     => true,
+            'edit_publication'              => true,
+            'delete_publication'            => true,
+            'read_publication'              => true,
         )
     );
 }
