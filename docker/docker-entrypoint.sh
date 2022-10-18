1c7741e0565851e2247bb4c0ac0b555bc76653bc
Ruben van der Linde <ruben@conduction.nl>
2022-10-18 16:19:23 +0200

    New docker files

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-set -e
+set -euo pipefail
 
 # first arg is `-f` or `--some-option`
-if [ "${1#-}" != "$1" ]; then
-	set -- php-fpm "$@"
+# or if our command is a valid wp-cli subcommand, let's invoke it through wp-cli instead
+# (this allows for "docker run wordpress:cli help", etc)
+if [ "${1#-}" != "$1" ] || wp help "$1" > /dev/null 2>&1; then
+	set -- wp "$@"
 fi
 
-echo "Container ready to recieve commands"
-
-exec docker-php-entrypoint "$@"
+exec "$@"
